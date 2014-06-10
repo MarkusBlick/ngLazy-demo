15c38e779ab7e05cd21297861a9120552f797182
Adam D Richman <ar@adamrichman.com>
2014-06-09 23:46:45 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('ngLazyDemoApp',['ngLazy'])
+  .service('dataService', ['$q', '$timeout', function($q, $timeout){
+      var getList = function(){    
+        var deferred = $q.defer();
+        $timeout(function(){
+          var string =  '<h4>List Item' + n + '</h4>' +
+                        '<p>' +
+                          'This is list item #' + n +
+                        '</p>';
+          var n = 0;
+          var list = [];
+          while (n < 1250) {
+            var string =  'This is a list item and it\'s ID is ' + n;
+            list.push(string);
+            n++;
+          }
+          var returnList = {};
+          returnList.data = {};
+          returnList.data.list = list;
+          deferred.resolve(returnList);
+        }, 3000);
+
+        return deferred.promise;
+      };
+      return { getList : getList };
+    }])
+  .controller('MainCtrl', ['$scope','dataService','$timeout', function ($scope, dataService, $timeout) {
+        
+        $scope.data = {};
+        $scope.data.list = []; 
+        $scope.range = '18';
+        $scope.startDelay = '150';
+        $scope.appendDelay = '2500';
+        $scope.spinnerColor = '#4FA7D9';
+
+        $scope.$watch('spinnerColor', function(newVal, oldVal){
+          $scope.spinnerColor = newVal;
+          console.log('watch', $scope.spinnerColor);
+        })
+        $scope.update = function(newVal){
+          $scope.spinnerColor = newVal;
+          $timeout(function(){
+            $scope.$apply(); 
+          },100)
+        }
+    }]);
