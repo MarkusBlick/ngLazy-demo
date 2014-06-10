d2c07879e4fb4581ed7db234f3c8da35e868e4b8
Adam D Richman <ar@adamrichman.com>
2014-06-10 14:06:47 -0700

    make jslint happy

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,21 +2,18 @@
 
 angular.module('ngLazyDemoApp',['ngLazy'])
   .service('dataService', ['$q', '$timeout', function($q, $timeout){
-      var getList = function(){    
+      var getList = function(){
         var deferred = $q.defer();
+        var n = 0;
+        var string = '';
         $timeout(function(){
-          var string =  '<h4>List Item' + n + '</h4>' +
-                        '<p>' +
-                          'This is list item #' + n +
-                        '</p>';
-          var n = 0;
           var list = [];
           while (n < 9001) {
-            var string =  'This is a list item and it\'s ID is ' + n;
+            string =  'This is a list item and it\'s ID is ' + n;
             list.push(string);
             n++;
           }
-          var string =  'It\'s over 9000';
+          string =  'It\'s over 9000';
           list.push(string);
           var returnList = {};
           returnList.data = {};
@@ -31,20 +28,21 @@ angular.module('ngLazyDemoApp',['ngLazy'])
   .controller('MainCtrl', ['$scope','dataService','$timeout', function ($scope, dataService, $timeout) {
         
         $scope.data = {};
-        $scope.data.list = []; 
+        $scope.data.list = [];
         $scope.range = '18';
         $scope.startDelay = '150';
         $scope.appendDelay = '2500';
         $scope.spinnerColor = '#4FA7D9';
 
-        $scope.$watch('spinnerColor', function(newVal, oldVal){
+        $scope.$watch('spinnerColor', function(newVal){
           $scope.spinnerColor = newVal;
           console.log('watch', $scope.spinnerColor);
-        })
+        });
+
         $scope.update = function(newVal){
           $scope.spinnerColor = newVal;
           $timeout(function(){
-            $scope.$apply(); 
-          },100)
-        }
-    }]);
+            $scope.$apply();
+          },100);
+        };
+      }]);
